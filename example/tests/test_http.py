210fe480da26b30893351683ea1bb87ef2162675
Jonatan S. Engstad <jonaengs@gmail.com>
2019-02-03 21:51:56 +0100

    add signup, login, logout, travis

--- example/tests/test_http.py
+++ example/tests/test_http.py
@@ -4,20 +4,29 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient, APITestCase
 
+"""
+To run these tests: python manage.py test example.tests
+"""
+
+PASSWORD = 'pAssw0rd!'
+
+
+def create_user(username='user@example.com', password=PASSWORD): # new
+    return get_user_model().objects.create_user(
+        username=username, password=password)
+
 
 class AuthenticationTest(APITestCase):
     def setUp(self):
         self.client = APIClient()
 
     def test_user_can_sign_up(self):
-        password = 'pAssw0rd!'
-        # NOTE: fields password1, password2 used in tutorial
         response = self.client.post(reverse('sign_up'), data={
             'username': 'user@example.com',
             'first_name': 'Test',
             'last_name': 'User',
-            'password': password,
-            'password_confirm': password,
+            'password1': PASSWORD,
+            'password2': PASSWORD,
         })
         user = get_user_model().objects.last()
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
@@ -25,3 +34,28 @@ class AuthenticationTest(APITestCase):
         self.assertEqual(response.data['username'], user.username)
         self.assertEqual(response.data['first_name'], user.first_name)
         self.assertEqual(response.data['last_name'], user.last_name)
+
+    def test_sign_up_wrong_confirmation_password(self):
+        response = self.client.post(reverse('sign_up'), data={
+            'username': 'user@example.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'password1': PASSWORD,
+            'password2': 'password2',
+        })
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+    def test_user_can_log_in(self):
+        user = create_user()
+        response = self.client.post(reverse('log_in'), data={
+            'username': user.username,
+            'password': PASSWORD,
+        })
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(response.data['username'], user.username)
+
+    def test_user_can_log_out(self):
+        user = create_user()
+        self.client.login(username=user.username, password=PASSWORD)
+        response = self.client.post(reverse('log_out'))
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
